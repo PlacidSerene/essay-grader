e5670ef7d1a7c11bf0245a26e2ab83e542bcad03
Daniel <t.dung.lhp@gmail.com>
2024-04-29 09:21:46 +0700

    add logo and user button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,73 @@
-"use client";
 import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { SignInButton, SignOutButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, auth, UserButton } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
-import { useUser } from "@clerk/clerk-react";
-
-const Navbar = () => {
-  const { isSignedIn } = useUser();
+import Image from "next/image";
+const Navbar = async () => {
+  const { userId } = auth();
   return (
     <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="z-40 flex font-semibold">
-            <span>quill.</span>
+            <Image
+              src="/chirp-logo-2.png"
+              width={100}
+              height={100}
+              alt={"chirp-logo"}
+              quality={100}
+            />
           </Link>
+          {/* <MobileNav isAuth={!!userId} /> */}
           <div className="hidden items-center space-x-4 font-semibold sm:flex">
             <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              >
-                Pricing
-              </Link>
-              {!isSignedIn && (
-                <SignInButton>
-                  <div
+              {!userId ? (
+                <>
+                  <Link
+                    href="/pricing"
                     className={buttonVariants({
                       variant: "ghost",
-                      size: "default",
+                      size: "sm",
                     })}
                   >
-                    Sign In
-                  </div>
-                </SignInButton>
-              )}
-              {isSignedIn && (
-                <SignOutButton>
-                  <div
+                    Pricing
+                  </Link>
+                  <SignInButton>
+                    <div
+                      className={buttonVariants({
+                        variant: "ghost",
+                        size: "default",
+                      })}
+                    >
+                      Sign In
+                    </div>
+                  </SignInButton>
+                  <SignUpButton>
+                    <div
+                      className={buttonVariants({
+                        size: "sm",
+                      })}
+                    >
+                      Get Started <ArrowRight className="ml-2 h-5 w-5" />
+                    </div>
+                  </SignUpButton>
+                </>
+              ) : (
+                <>
+                  <Link
+                    href="/dashboard"
                     className={buttonVariants({
                       variant: "ghost",
-                      size: "default",
+                      size: "sm",
                     })}
                   >
-                    Sign Out
-                  </div>
-                </SignOutButton>
+                    Dashboard
+                  </Link>
+                  <UserButton />
+                </>
               )}
-              <SignUpButton>
-                <div
-                  className={buttonVariants({
-                    size: "sm",
-                  })}
-                >
-                  Get Started <ArrowRight className="ml-2 h-5 w-5" />
-                </div>
-              </SignUpButton>
             </>
           </div>
         </div>
